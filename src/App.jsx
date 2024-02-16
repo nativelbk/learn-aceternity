e047ae450d382700c0898b39c0ad49a903022b4d
Nativel <nativelbk@gmail.com>
2024-02-16 21:11:57 +0100

    add 3d card

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import ThreeDCardDemo from "./components/3dCar";
+
+/** @format */
+export default function App() {
+  return (
+    <>
+      <ThreeDCardDemo />
+    </>
+  );
+}
---
+++ src/utils/cn.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
