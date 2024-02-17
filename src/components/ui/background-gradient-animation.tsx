c37d5f1a5a3ab4e13c0e91f5c5b529563b4de4e3
Nativel <nativelbk@gmail.com>
2024-02-17 21:50:56 +0100

    Change background

--- src/components/ui/background-gradient-animation.tsx
+++ src/components/ui/background-gradient-animation.tsx
@@ -86,7 +86,7 @@ export const BackgroundGradientAnimation = ({
   return (
     <div
       className={cn(
-        "h-screen w-screen relative overflow-x-hidden overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
+        "h-screen w-screen relative overflow-x-hidden overflow-hidden top-0 left-0 ",
         containerClassName
       )}
     >
